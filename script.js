9e117ff25bf7a9e6b8a30825cadcb4a7695bc687
JocelinPrevost <114737976+JocelinPrevost@users.noreply.github.com>
2023-01-15 22:06:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const changecarton=()=> {
+  var carton = document.getElementById("carton");
+  var secondImage = document.getElementById("carton ouvert");
+  var video = document.getElementById("video");
+  var body = document.getElementById("body")
+  if (carton.src.match("carton")) {
+    carton.src = "carton ouvert.png";
+    body.classList.add("fond")
+    setTimeout(function() {
+    video.style.display = "block";
+    secondImage.style.display = "none";
+    video.play();
+  }, 4000);
+  } else {
+    carton.src = "carton.jpg";
+    video.style.display = "none";
+    secondImage.style.display = "block";
+  }
+}
+
+
+alert("Baisse le son")
